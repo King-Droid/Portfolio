7cf099609211dbf79f66ffd12d08ed98f3c2788c
yadav.naresh07@gmail.com <ncy@123#>
2020-11-29 20:32:26 +0530

    Initial Commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,21 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'Naresh';
+  static const String me = ' Yadav';
+  static const String portfoli = 'Portfoli';
+  static const String o = 'o';
+  static const String headline_part1 = 'Mobile App Developer (Android,Flutter)';
+  static const String headline_part2 = 'Delhi-NCR, India';
+  static const String summary = 'Focused professional having excellent technical and communication skills, and offering 7 years of experience in Mobile application development. Proficient at designing and developing Android native and Flutter Hybrid apps development. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved = '© 2020 Portfolio. ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_about = 'About';
+  static const String menu_projects = 'Projects';
+  static const String menu_contact = 'Contact';
+
+}
